76da7f723cdf8e96d32124bb44d01456529da1db
Nyfaasmin <140721957+Nyfaasmin@users.noreply.github.com>
2025-05-16 16:29:12 +0530

    Add files via upload

---
+++ video-to-gif-generator/utils/whisper_transcriber.py
@@ -0,0 +1,6 @@
+def transcribe_video(video_path, prompt):
+    # Dummy transcript
+    transcript = ["This is a motivational line.", "Another funny quote here."]
+    # Dummy segments
+    segments = [(0, 3), (4, 7)]
+    return transcript, segments
